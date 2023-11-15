64b2752eb9a4460821ec58c59de4e228e0c83f02
Edwards Presume <edwardspresume@gmail.com>
2023-11-14 22:34:17 -0500

    Add OpenAI image analysis using GPT-4 vision model

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,24 +1,110 @@
+import { SECRET_OPENAI_API_KEY } from '$env/static/private';
 import type { Actions, PageServerLoad } from './$types';
 
-import { superValidate } from 'sveltekit-superforms/server';
+import { message, superValidate } from 'sveltekit-superforms/server';
 
-import { ImageSchema } from '$lib';
+import { ImageDescriptionSchema } from '$lib';
+
+import { z } from 'zod';
+
+import OpenAI from 'openai';
+
+import { Buffer } from 'buffer'; // Import the Buffer module
+
+type AnalysisData = {
+	imageBase64: string;
+	description: string;
+};
+
+async function analyzeImage({ imageBase64, description }: AnalysisData) {
+	const openai = new OpenAI({
+		apiKey: SECRET_OPENAI_API_KEY
+	});
+
+	const response: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create({
+		model: 'gpt-4-vision-preview', // Make sure to use the correct vision model name
+		messages: [
+			{
+				role: 'user',
+				content: [
+					{ type: 'text', text: description },
+					{
+						type: 'image_url',
+						image_url: {
+							url: imageBase64
+						}
+					}
+				]
+			}
+		]
+	});
+
+	console.log(response.choices[0].message.content);
+	return response.choices[0].message.content;
+}
+
+const ImageSchema = z.object({
+	image: z
+		.instanceof(File)
+		.refine((file) => file.type.startsWith('image/'), 'Uploaded file is not an image')
+});
 
 export const load = (async () => {
 	return {
-		imageForm: superValidate(ImageSchema)
+		imageForm: superValidate(ImageDescriptionSchema)
 	};
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-	default: async (request) => {
-		console.log(request);
+	default: async ({ request }) => {
+		const formData = await request.formData();
+		const imageForm = await superValidate(formData, ImageDescriptionSchema);
 
-		return {
-			status: 200,
-			body: {
-				message: 'Hello world!'
-			}
-		};
+		const imageFile = formData.get('image') as File | null;
+
+		if (!imageForm.valid) {
+			return message(imageForm, {
+				alertType: 'error',
+				alertMessage: 'Invalid description'
+			});
+		}
+
+		const validationResult = ImageSchema.safeParse({ image: imageFile });
+
+		if (!validationResult.success) {
+			return message(imageForm, {
+				alertType: 'error',
+				alertMessage: validationResult.error.issues
+			});
+		}
+
+		if (!imageFile) return;
+		// GPT change - Convert the Blob to a Buffer and then to a base64 string
+		const imageBuffer = Buffer.from(await imageFile.arrayBuffer());
+		const base64Image = imageBuffer.toString('base64');
+
+		try {
+			const openAiResponse = await analyzeImage({
+				imageBase64: `data:image/jpeg;base64,${base64Image}`,
+				description: imageForm.data.description
+			});
+
+			return message(imageForm, {
+				alertType: 'success',
+				alertMessage: openAiResponse
+			});
+		} catch (error) {
+			console.error('Error analyzing image:', error);
+			return message(
+				imageForm,
+				{
+					alertType: 'error',
+					alertMessage: 'Error analyzing image'
+				},
+				{
+					status: 500
+				}
+			);
+		}
 	}
 };
