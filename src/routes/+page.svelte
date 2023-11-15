cad92a92a93039e8d3d4cc7cfd644230e6590a08
Edwards Presume <edwardspresume@gmail.com>
2023-11-15 06:46:56 -0500

    Remove clipboard import and add copy to clipboard functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
-	import { FileDropzone, clipboard } from '@skeletonlabs/skeleton';
+	import { FileDropzone } from '@skeletonlabs/skeleton';
 
 	import { IconCopy, IconLoader2 } from '@tabler/icons-svelte';
 
@@ -16,16 +16,26 @@
 		validators: ImageDescriptionSchema
 	});
 
-	let uploadedFile;
 	let uploadedFileUrl: string | null = null;
 	$: openAiResponse = '';
 
 	function handleFileUpload(event: Event) {
 		const input = event.target as HTMLInputElement;
 		if (input.files) {
-			uploadedFile = input.files[0];
-			uploadedFileUrl = URL.createObjectURL(uploadedFile);
+			uploadedFileUrl = URL.createObjectURL(input.files[0]);
 		}
+	}
+
+	let copied = false;
+
+	function copyToClipboard() {
+		navigator.clipboard.writeText(openAiResponse);
+
+		copied = true;
+
+		setTimeout(() => {
+			copied = false;
+		}, 1000);
 	}
 
 	$: if ($message && $message.alertType === 'success') {
@@ -39,73 +49,75 @@
 	$: $form.description = `Please describe this image with a focus on the fashion and aesthetics`;
 </script>
 
-<main class="p-2 grid py-10 place-content-center w-full">
-	<div class="max-w-4xl flex flex-col md:flex-row gap-10">
-		<section class="flex-1">
-			<h1 class="text-2xl text-center mb-8">Image description generator</h1>
+<main class="grid max-w-4xl gap-10 p-3 py-10 mx-auto justify-center sm:grid-cols-[auto,auto]">
+	<section class="flex-1">
+		<h1 class="mb-8 text-2xl text-center">Image description generator</h1>
 
-			{#if $message && $message.alertType === 'error'}
-				<p class="text-center text-red-500 mb-5">{$message.alertMessage}</p>
-			{/if}
+		{#if $message && $message.alertType === 'error'}
+			<p class="mb-5 text-center text-red-500">{$message.alertMessage}</p>
+		{/if}
 
-			<form method="POST" use:enhance class="grid gap-9">
-				<FileDropzone
-					name="image"
-					accept="image/*"
-					enctype="multipart/form-data"
-					on:change={handleFileUpload}
-				>
-					<svelte:fragment slot="lead">
-						{#if uploadedFileUrl}
-							<img
-								src={uploadedFileUrl}
-								alt="Uploaded Preview"
-								width="200"
-								height="200"
-								class="object-cover block mx-auto rounded"
-							/>
-						{:else}
-							🖼️
-						{/if}
-					</svelte:fragment>
-					<svelte:fragment slot="message">Upload image or drag and drop</svelte:fragment>
-				</FileDropzone>
-
-				<label class="label">
-					<span>Enter prompt</span>
-					<p class="text-red-500">{$errors.description ? $errors.description : ''}</p>
-					<textarea
-						rows="8"
-						class="textarea"
-						name="description"
-						placeholder="Describe the image focusing on fashion and aesthetics..."
-						bind:value={$form.description}
-					/>
-				</label>
-
-				<button type="submit" class="btn variant-filled">
-					{#if $delayed}
-						<IconLoader2 class="animate-spin" />
+		<form method="POST" use:enhance class="grid gap-9">
+			<FileDropzone
+				name="image"
+				accept="image/*"
+				enctype="multipart/form-data"
+				on:change={handleFileUpload}
+			>
+				<svelte:fragment slot="lead">
+					{#if uploadedFileUrl}
+						<img
+							src={uploadedFileUrl}
+							alt="Uploaded Preview"
+							width="150"
+							height="150"
+							class="block object-cover mx-auto rounded"
+						/>
+					{:else}
+						🖼️
 					{/if}
+				</svelte:fragment>
+				<svelte:fragment slot="message">Upload image or drag and drop</svelte:fragment>
+			</FileDropzone>
 
-					<span> Submit </span>
-				</button>
-			</form>
-		</section>
+			<label class="label">
+				<span>Enter prompt</span>
+				<p class="text-red-500">{$errors.description ? $errors.description : ''}</p>
+				<textarea
+					rows="8"
+					class="textarea"
+					name="description"
+					placeholder="Describe the image focusing on fashion and aesthetics..."
+					bind:value={$form.description}
+				/>
+			</label>
 
-		{#if openAiResponse}
-			<section class="flex-1">
-				<h2 class="text-2xl text-center mb-8">Response</h2>
+			<button type="submit" class="btn variant-filled">
+				{#if $delayed}
+					<IconLoader2 class="animate-spin" />
+				{/if}
 
-				<div class="bg-gray-700 p-4 rounded shadow space-y-2">
-					<button class="flex gap-2" use:clipboard={openAiResponse}>
+				<span> Submit </span>
+			</button>
+		</form>
+	</section>
+
+	{#if openAiResponse}
+		<section class="flex-1">
+			<h2 class="mb-8 text-2xl text-center">Response</h2>
+
+			<div class="p-4 space-y-2 bg-gray-700 rounded max-h-[600px] overflow-hidden overflow-y-auto">
+				<button class="flex gap-2" on:click={copyToClipboard}>
+					{#if copied}
+						Copied 👍
+					{:else}
 						<IconCopy />
 						<span>Copy to Clipboard</span>
-					</button>
+					{/if}
+				</button>
 
-					<p class="whitespace-pre-wrap">{openAiResponse}</p>
-				</div>
-			</section>
-		{/if}
-	</div>
+				<p class="whitespace-pre-wrap">{openAiResponse}</p>
+			</div>
+		</section>
+	{/if}
 </main>
