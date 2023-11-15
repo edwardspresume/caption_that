8239c2191e024ffa51abc5c25f204d7438d67d9a
Edwards Presume <edwardspresume@gmail.com>
2023-11-14 21:09:25 -0500

    Refactor ImageSchema to ImageDescriptionSchema

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,5 @@
 import { z } from 'zod';
 
-export const ImageSchema = z.object({
-	image: z.instanceof(File).refine((file) => file.type.startsWith('image/'), {
-		message: 'Uploaded file is not an image'
-	}),
+export const ImageDescriptionSchema = z.object({
 	description: z.string().min(1, { message: 'Description is required' })
 });
