a854b12205d4c8f87fc651082e2e3280093c3574
Edwards Presume <edwardspresume@gmail.com>
2023-11-26 16:26:09 -0500

    feat(StarredTitle.svelte): add StarredTitle component to display a star icon before and after the content

---
+++ src/lib/components/StarredTitle.svelte
@@ -0,0 +1,5 @@
+<iconify-icon icon="mdi:stars" class="text-[#fdd932]"></iconify-icon>
+<span>
+	<slot />
+</span>
+<iconify-icon icon="mdi:stars" class="text-[#fdd932] rotate-180"></iconify-icon>
--- src/lib/components/siteMainHeader/SiteMainHeader.svelte
+++ src/lib/components/siteMainHeader/SiteMainHeader.svelte
@@ -1,20 +1,15 @@
 <script>
-	import Button from '$components/ui/button/button.svelte';
 	import { Routes } from '$lib/navLinks';
 
+	import StarredTitle from '$components/StarredTitle.svelte';
+	import Button from '$components/ui/button/button.svelte';
 	import ThemeToggler from './ThemeToggler.svelte';
 </script>
 
 <header class="relative mb-8 border-b">
 	<div class="container flex items-center justify-between p-2">
-		<Button
-			href={Routes.HOMEPAGE}
-			variant="ghost"
-			class="gap-1 p-0 text-base font-bold sm:text-2xl"
-		>
-			<span class="rotate-180">✨</span>
-			<span>Image Caption Creator</span>
-			<span class="-scale-y-100">✨</span>
+		<Button href={Routes.HOMEPAGE} variant="ghost" class="gap-1 p-0 text-base font-bold sm:text-xl">
+			<StarredTitle>CaptionThat</StarredTitle>
 		</Button>
 
 		<ThemeToggler />
