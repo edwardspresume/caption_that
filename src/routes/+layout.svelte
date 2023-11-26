e86b91a23276d73019eba3efc9c309adb1adf6f0
Edwards Presume <edwardspresume@gmail.com>
2023-11-26 16:45:09 -0500

    chore(layout.svelte): add analytics injection and mode configuration for development and production environments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
 	import '../app.pcss';
 
 	import { Bar } from '@bobbymannino/svelte-progress';
@@ -6,6 +9,8 @@
 	import { ModeWatcher } from 'mode-watcher';
 
 	import SiteMainHeader from '$components/siteMainHeader/SiteMainHeader.svelte';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Bar color="#6D28D9" size="big" speed="fast" />
