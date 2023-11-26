8c000e1640820ad4e32f3c44b81f9b9ff8fc1cb0
Edwards Presume <edwardspresume@gmail.com>
2023-11-26 13:47:16 -0500

    Add ModeWatcher component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,14 @@
 
 	import { Bar } from '@bobbymannino/svelte-progress';
 
+	import { ModeWatcher } from 'mode-watcher';
+
 	import SiteMainHeader from '$components/siteMainHeader/SiteMainHeader.svelte';
 </script>
 
 <Bar color="#6D28D9" size="big" speed="fast" />
+
+<ModeWatcher />
 
 <SiteMainHeader />
 
