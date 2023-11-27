7eb93077b51a5d5f1c88cb4930f70a44afea25d8
Edwards Presume <edwardspresume@gmail.com>
2023-11-27 06:05:06 -0500

    Reset caption after uploading an image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,6 +42,8 @@
 				// Revoke the old URL if it exists
 				uploadedImageUrl && URL.revokeObjectURL(uploadedImageUrl);
 				uploadedImageUrl = URL.createObjectURL(file);
+
+				generatedCaption = '';
 			} catch (e) {
 				toast.error('Failed to upload image. Please try again.');
 			} finally {
