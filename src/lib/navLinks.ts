819a98780604f7b43353255922b0c96e1810464f
Edwards Presume <edwardspresume@gmail.com>
2024-01-08 15:53:24 -0500

    Refactor navigation links in navLinks.ts

--- src/lib/navLinks.ts
+++ src/lib/navLinks.ts
@@ -1,9 +1,5 @@
 import type { AriaAttributes } from 'svelte/elements';
-
-// Enum for route paths
-export enum Routes {
-	HOMEPAGE = '/'
-}
+import { route } from './ROUTES';
 
 // Type for navigation link
 export type NavLink = {
@@ -13,32 +9,29 @@ export type NavLink = {
 	isExternal?: boolean;
 };
 
-/**
- * Create a NavLink object for use in navigation menus.
- *
- * @param title - The text to display for the navigation link.
- * @param path - The route path from the Routes enum that this link should navigate to.
- * @param ariaLabel - (Optional) The aria-label for the link, for accessibility. Defaults to the title if not provided.
- * @param isExternal - (Optional) Flag to indicate if the link is external to the application's routing context (e.g., linking to an external site).
- * @returns The NavLink object with the provided properties.
- * @throws Will throw an error if title or path is not provided.
- */
-export const createNavLink = (
-	title: string,
-	path: Routes,
-	ariaLabel?: AriaAttributes['aria-label'],
-	isExternal: boolean | undefined = false
-): NavLink => {
-	if (!title || !path) {
-		throw new Error('Title and path are required to create a NavLink');
+export const createNavLink = ({ title, href, ariaLabel, isExternal }: NavLink) => {
+	if (!title || !href) {
+		throw new Error('Title and href are required to create a NavLink');
 	}
 
 	return {
 		title,
-		href: path,
-		ariaLabel: ariaLabel || title,
+		href,
+		ariaLabel: ariaLabel ?? title,
 		isExternal
 	};
 };
 
-export const mainNavLinks: NavLink[] = [createNavLink('Home', Routes.HOMEPAGE, 'Go to homepage')];
+export const mainNavLinks = {
+	home: createNavLink({
+		title: 'Home',
+		href: route('/'),
+		ariaLabel: 'Go to home page'
+	}),
+
+	app: createNavLink({
+		title: 'App',
+		href: route('/app'),
+		ariaLabel: 'Go to app page'
+	})
+} as const;
