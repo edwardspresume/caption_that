b2953fad8e24b984baafa15042d4ca768cfa207b
Edwards Presume <edwardspresume@gmail.com>
2024-01-08 16:07:59 -0500

    Refactor caption form and component imports

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,39 +1,30 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 
-	import { currentCaption } from '$lib/store';
-
-	import StarredTitle from '$components/appPage/StarredTitle.svelte';
-	import CaptionContextForm from '$components/appPage/form/CaptionContextForm.svelte';
+	import StarredTitle from '$components/StarredTitle.svelte';
+	import CaptionCreationForm, { currentCaption } from '$components/appPage/form/CaptionCreationForm.svelte';
 	import Button from '$components/ui/button/button.svelte';
 
 	export let data: PageData;
 
+
 	let captionCopied = false;
-	let captionSectionRef: HTMLElement;
+      let captionSectionRef: HTMLElement;
 
-	$: if ($currentCaption && captionSectionRef) {
-		captionSectionRef.scrollIntoView();
-	}
+      $: if ($currentCaption && captionSectionRef) {
+            captionSectionRef.scrollIntoView();
+      }
 
-	function copyCaptionClipboard() {
-		navigator.clipboard.writeText($currentCaption);
+      function copyCaptionClipboard() {
+            navigator.clipboard.writeText($currentCaption);
 
-		captionCopied = true;
+            captionCopied = true;
 
-		setTimeout(() => {
-			captionCopied = false;
-		}, 2000);
-	}
+            setTimeout(() => {
+                  captionCopied = false;
+            }, 2000);
+      }
 </script>
-
-<svelte:head>
-	<title>Image Caption Generator | CaptionThat</title>
-	<meta
-		name="description"
-		content="CaptionThat is a web application that generates unique and descriptive captions for your images using OpenAI GPT-4 Vision"
-	/>
-</svelte:head>
 
 <main class="container flex-1 max-w-xl p-2 pb-10">
 	<header class="mb-5 text-center">
@@ -46,26 +37,38 @@
 		</p>
 	</header>
 
-	<CaptionContextForm captionContextForm={data.captionContextForm} />
+	<CaptionCreationForm captionCreationForm={data.captionCreationForm} />
 
 	{#if $currentCaption}
-		<section class="p-4 mt-10 border rounded-md" aria-live="polite" bind:this={captionSectionRef}>
-			<div class="flex items-center justify-between mb-5">
-				<h2 class="text-xl font-bold">Caption</h2>
+		  <section
+				class="p-4 mt-10 border rounded-md"
+				aria-live="polite"
+				bind:this={captionSectionRef}
+		  >
+				<div class="flex items-center justify-between mb-5">
+					  <h2 class="text-xl font-bold">Caption</h2>
 
-				<Button class="flex items-center gap-1" on:click={copyCaptionClipboard}>
-					{#if captionCopied}
-						Copied 👍
-					{:else}
-						<iconify-icon icon="akar-icons:copy" class="text-lg"></iconify-icon>
-						<span>Copy to Clipboard</span>
-					{/if}
-				</Button>
-			</div>
+					  <Button
+							class="flex items-center gap-1"
+							on:click={copyCaptionClipboard}
+					  >
+							{#if captionCopied}
+								  Copied 👍
+							{:else}
+								  <iconify-icon
+										icon="akar-icons:copy"
+										class="text-lg"
+								  ></iconify-icon>
+								  <span>Copy to Clipboard</span>
+							{/if}
+					  </Button>
+				</div>
 
-			<p class="p-3 bg-accent rounded-md max-h-[400px] shadow-md overflow-hidden overflow-y-auto">
-				{$currentCaption}
-			</p>
-		</section>
+				<p
+					  class="p-3 bg-accent rounded-md max-h-[400px] shadow-md overflow-hidden overflow-y-auto"
+				>
+					  {$currentCaption}
+				</p>
+		  </section>
 	{/if}
 </main>
