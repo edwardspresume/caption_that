3ddedbe28b5535e9970a305db13d6b3b3afe2039
Edwards Presume <edwardspresume@gmail.com>
2024-01-08 17:31:45 -0500

    Update home page test and remove basic test

--- tests/e2e/homePage.test.ts
+++ tests/e2e/homePage.test.ts
@@ -1,19 +1,25 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('Basic home page tests', () => {
-	test('verify page heading and toast message', async ({ page }) => {
+	test('Verify page headings and navigation', async ({ page }) => {
 		// Navigate to the home page
 		await page.goto('/');
 
-		// Check if the heading 'Home page' is visible
-		const heading = page.getByRole('heading', { name: 'Home page' });
-		await expect(heading).toBeVisible();
+		// Check if the heading is visible
+		const homePageHeading = page.getByRole('heading', {
+			name: 'CaptionThat - Unleash Your Creativity'
+		});
+		await expect(homePageHeading).toBeVisible();
 
-		// Click the button that shows the toast
-		const showToastButton = page.getByRole('button', { name: 'Show toast' });
-		await showToastButton.click();
+		// Click the link that navigates to "/app"
+		const link = page.getByRole('link', { name: 'Try it out for free' });
+		await link.first().click();
 
-		// Verify the toast message 'Hello world' is displayed
-		await expect(page.locator('text=Hello world!')).toBeVisible();
+		await page.waitForURL('/app');
+
+		const appPageHeading = page.getByRole('heading', {
+			name: 'Image Caption Creator'
+		});
+		await expect(appPageHeading).toBeVisible();
 	});
 });
