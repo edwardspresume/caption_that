517882479bd39173a1f35e8d4425e5b509534223
Edwards Presume <edwardspresume@gmail.com>
2024-01-08 16:21:03 -0500

    Add ImageCarousel component to landing page

--- src/lib/components/landingPage/ImageCarousel.svelte
+++ src/lib/components/landingPage/ImageCarousel.svelte
@@ -20,7 +20,7 @@
 	import image_6_desktop from '$lib/assets/slider_images/6_desktop.jpg';
 	import image_6_mobile from '$lib/assets/slider_images/6_mobile.jpg';
 
-	const images: Image[] = [
+	const carouselImages: Image[] = [
 		{
 			mobile_src: image_1_mobile,
 			desktop_src: image_1_desktop,
@@ -54,35 +54,46 @@
 		}
 	];
 
-	const INTERVAL_TIME = 3000;
+	// Time between image changes in milliseconds
+	const INTERVAL_TIME_MS = 3000;
+
+	// Index of the currently displayed image
 	const currentImageIndex = writable(0);
 
-	let interval: ReturnType<typeof setInterval>;
+	// ID of the interval used for changing images
+	let imageChangeIntervalId: ReturnType<typeof setInterval>;
 
-	const updateImage = () => {
-		$currentImageIndex = ($currentImageIndex + 1) % images.length;
+	// Function to advance to the next image
+	const goToNextImage = () => {
+		$currentImageIndex = ($currentImageIndex + 1) % carouselImages.length;
 	};
 
-	const startInterval = () => {
-		interval = setInterval(updateImage, INTERVAL_TIME);
+	// Function to start automatically advancing to the next image
+	const startImageChangeInterval = () => {
+		imageChangeIntervalId = setInterval(goToNextImage, INTERVAL_TIME_MS);
 	};
 
-	const clearExistingInterval = () => {
-		if (interval) clearInterval(interval);
+	// Function to stop automatically advancing to the next image
+	const stopImageChangeInterval = () => {
+		if (imageChangeIntervalId) clearInterval(imageChangeIntervalId);
 	};
 
-	onMount(startInterval);
-	onDestroy(clearExistingInterval);
+	// Start automatically advancing to the next image when the component is mounted
+	onMount(startImageChangeInterval);
 
-	const toggleInterval = (shouldPause: boolean) => {
-		shouldPause ? clearExistingInterval() : startInterval();
+	// Stop automatically advancing to the next image when the component is destroyed
+	onDestroy(stopImageChangeInterval);
+
+	// Function to start or stop automatically advancing to the next image
+	const toggleImageChangeInterval = (shouldPause: boolean) => {
+		shouldPause ? stopImageChangeInterval() : startImageChangeInterval();
 	};
 </script>
 
-{#if images.length > 0}
-	<figure class="w-full">
+{#if carouselImages.length > 0}
+	<figure>
 		<div class="relative aspect-[16/12]">
-			{#each [images[$currentImageIndex]] as image (image?.mobile_src)}
+			{#each [carouselImages[$currentImageIndex]] as image (image?.mobile_src)}
 				<picture>
 					<source media="(min-width: 768px)" srcset={image?.desktop_src} />
 					<source media="(max-width: 767px)" srcset={image?.mobile_src} />
@@ -90,8 +101,8 @@
 						src={image?.mobile_src}
 						alt={image?.caption}
 						transition:fade={{ duration: 800 }}
-						on:mouseenter={() => toggleInterval(true)}
-						on:mouseleave={() => toggleInterval(false)}
+						on:mouseenter={() => toggleImageChangeInterval(true)}
+						on:mouseleave={() => toggleImageChangeInterval(false)}
 						class="absolute object-cover w-full h-full duration-300 rounded-md hover:scale-95"
 					/>
 				</picture>
@@ -99,17 +110,17 @@
 		</div>
 
 		<figcaption
-			class="p-2 mx-auto mt-4 italic font-medium text-center rounded bg-accent w-fit text-pretty"
+			class="p-2 mx-auto mt-4 italic font-medium text-center rounded-lg w-fit text-pretty bg-accent"
 		>
-			{images[$currentImageIndex]?.caption}
+			{carouselImages[$currentImageIndex]?.caption}
 		</figcaption>
 	</figure>
 {/if}
 
 <style>
 	img {
-		--mask: linear-gradient(#000 0 0) 50% / calc(100% - 55.5px) calc(100% - 55.5px) no-repeat,
-			radial-gradient(farthest-side, #000 98%, #0000) 0 0/60px 60px round;
+		--mask: linear-gradient(#000 0 0) 50% / calc(100% - 40px) calc(100% - 40px) no-repeat,
+			radial-gradient(farthest-side, #000 98%, #0000) 0 0/70px 70px round;
 		-webkit-mask: var(--mask);
 		mask: var(--mask);
 	}
