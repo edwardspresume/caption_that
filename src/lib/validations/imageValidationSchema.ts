c7e30b61d4dfde0a1c1512dc5783d98f4db720c8
Edwards Presume <edwardspresume@gmail.com>
2024-01-08 16:39:55 -0500

    refactor(imageValidationSchema.ts): change SUPPORTED_IMAGE_TYPES from object to array for better readability and maintainability

--- src/lib/validations/imageValidationSchema.ts
+++ src/lib/validations/imageValidationSchema.ts
@@ -3,20 +3,20 @@ import { z } from 'zod';
 
 export const MAX_FILE_SIZE_MB = 20;
 
-export const SUPPORTED_IMAGE_TYPES: Record<string, keyof sharp.FormatEnum> = {
-	'image/jpeg': 'jpeg',
-	'image/png': 'png',
-	'image/webp': 'webp',
-	'image/avif': 'avif',
-	'image/gif': 'gif'
-};
+export const SUPPORTED_IMAGE_TYPES: Array<keyof sharp.FormatEnum> = [
+	'jpeg',
+	'png',
+	'webp',
+	'avif',
+	'gif'
+];
 
 export const IMAGE_VALIDATION_ERROR_MESSAGES = {
 	noFileUploaded: 'Please upload a file.',
 	notAnImage: 'The file you uploaded is not an image.',
-	unsupportedImageType: `The image type you uploaded is not supported. Please upload an image of one of the following types: ${Object.values(
-		SUPPORTED_IMAGE_TYPES
-	).join(', ')}.`,
+	unsupportedImageType: `The image type you uploaded is not supported. Please upload an image of one of the following types: ${SUPPORTED_IMAGE_TYPES.join(
+		', '
+	)}.`,
 	fileTooLarge: `The file size must be less than ${MAX_FILE_SIZE_MB} MB.`
 };
 
@@ -25,9 +25,15 @@ export const imageValidationSchema = z.object({
 		.instanceof(File)
 		.refine((file) => file.size > 0, IMAGE_VALIDATION_ERROR_MESSAGES.noFileUploaded)
 		.refine((file) => file.type.startsWith('image/'), IMAGE_VALIDATION_ERROR_MESSAGES.notAnImage)
-		.refine((file) => SUPPORTED_IMAGE_TYPES[file.type], {
-			message: IMAGE_VALIDATION_ERROR_MESSAGES.unsupportedImageType
-		})
+		.refine(
+			(file) => {
+				const imageType = file.type.split('/')[1];
+				return imageType ? SUPPORTED_IMAGE_TYPES.includes(imageType) : false;
+			},
+			{
+				message: IMAGE_VALIDATION_ERROR_MESSAGES.unsupportedImageType
+			}
+		)
 		.refine((file) => file.size <= MAX_FILE_SIZE_MB * 1024 * 1024, {
 			message: IMAGE_VALIDATION_ERROR_MESSAGES.fileTooLarge
 		})
